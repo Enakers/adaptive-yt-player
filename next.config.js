17383838be16a00e79aaee7e047a211815a51375
EnakNalla <enak.nalla@gmail.com>
2022-08-05 20:05:06 +0100

    add security headers with next-safe

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
+const nextSafe = require("next-safe");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  headers: () => [
+    {
+      source: "/:path*",
+      headers: [
+        ...nextSafe({
+          isDev: process.env.NODE_ENV !== "production",
+          contentSecurityPolicy: {
+            "img-src": [
+              "'self'",
+              "data:",
+              "https://lh3.googleusercontent.com",
+              "https://i.ytimg.com",
+              "https://yt3.ggpht.com"
+            ],
+            "connect-src": ["'self'", "https://www.googleapis.com"],
+            "script-src": ["'self'", "https://www.youtube.com"],
+            "frame-src": ["'self'", "https://www.youtube-nocookie.com"]
+          },
+          permissionsPolicy: {
+            fullscreen: ["'self'"]
+          }
+        }),
+        {
+          key: "Strict-Transport-Security",
+          value: "max-age=31536000; includeSubDomains; preload"
+        }
+      ]
+    }
+  ]
 };
 
 module.exports = nextConfig;
