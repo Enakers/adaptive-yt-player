a546aae1aa0075492b2946e4220f3da2d9ef08c5
EnakNalla <enak.nalla@gmail.com>
2022-08-05 23:42:50 +0100

    disable nextSafe temporarily
    csp causing iframe to fail for select videos.
    Disable while investigating.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const nextSafe = require("next-safe");
 
+const isDev = process.env.NODE_ENV !== "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,25 +9,27 @@ const nextConfig = {
     {
       source: "/:path*",
       headers: [
-        ...nextSafe({
-          isDev: process.env.NODE_ENV !== "production",
-          contentSecurityPolicy: {
-            "img-src": [
-              "'self'",
-              "data:",
-              "https://lh3.googleusercontent.com",
-              "https://i.ytimg.com",
-              "https://yt3.ggpht.com"
-            ],
-            "connect-src": ["'self'", "https://www.googleapis.com"],
-            "script-src": ["'self'", "https://www.youtube.com"],
-            "frame-src": ["'self'", "https://www.youtube-nocookie.com"],
-            "style-src": ["'self'", "sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU="]
-          },
-          permissionsPolicy: {
-            fullscreen: ["'self'"]
-          }
-        }),
+        // ...nextSafe({
+        //   isDev,
+        //   contentSecurityPolicy: {
+        //     "img-src": [
+        //       "'self'",
+        //       "data:",
+        //       "https://lh3.googleusercontent.com",
+        //       "https://i.ytimg.com",
+        //       "https://yt3.ggpht.com"
+        //     ],
+        //     "connect-src": ["'self'", "https://www.googleapis.com"],
+        //     "script-src": ["'self'", "https://www.youtube.com"],
+        //     "frame-src": ["'self'", "https://www.youtube-nocookie.com"],
+        //     "style-src": isDev
+        //       ? []
+        //       : ["'self'", "'sha256-+ggG/X5/BziMV+92NuWVuGj96UXUoD0vzVERtSh8Nv4='"]
+        //   },
+        //   permissionsPolicy: {
+        //     fullscreen: ["'self'"]
+        //   }
+        // }),
         {
           key: "Strict-Transport-Security",
           value: "max-age=31536000; includeSubDomains; preload"
