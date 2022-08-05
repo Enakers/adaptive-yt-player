7632deacc9d3e624fa9e4ed863c0bcd1cf14f28b
EnakNalla <enak.nalla@gmail.com>
2022-08-05 19:39:58 +0100

    improve input selector styling

--- src/components/Input/InputSelect.tsx
+++ src/components/Input/InputSelect.tsx
@@ -1,16 +1,16 @@
-import { observer } from 'mobx-react-lite';
-import { Card, Col, Container, Row } from 'react-bootstrap';
-import { useStore } from '~/store/StoreProvider';
+import {observer} from "mobx-react-lite";
+import {Card, Col, Container, Row} from "react-bootstrap";
+import {useStore} from "~/store/StoreProvider";
 
 function InputSelect() {
-  const { inputStore } = useStore();
+  const {inputStore} = useStore();
 
   return (
     <Container>
-      <h4 className="text-center">Please select an input method</h4>
-      <Row>
-        <Col>
-          <Card role="button" onClick={() => inputStore.setInputMethod('switch')}>
+      <h4 className="text-center mb-4">Please select an input method</h4>
+      <Row className="g-4" xs="1" md="2" lg="4">
+        <Col className="mx-auto">
+          <Card role="button" onClick={() => inputStore.setInputMethod("switch")}>
             <Card.Img variant="top" src="/switch.png" alt="Switch symbol" />
             <Card.Body>
               <Card.Title className="text-center">Switch</Card.Title>
@@ -19,7 +19,7 @@ function InputSelect() {
         </Col>
 
         <Col>
-          <Card role="button" onClick={() => inputStore.setInputMethod('eye gaze')}>
+          <Card role="button" onClick={() => inputStore.setInputMethod("eye gaze")}>
             <Card.Img variant="top" src="/eye.png" alt="Eye symbol" />
             <Card.Body>
               <Card.Title className="text-center">Eye gaze</Card.Title>
@@ -28,7 +28,7 @@ function InputSelect() {
         </Col>
 
         <Col>
-          <Card role="button" onClick={() => inputStore.setInputMethod('mouse')}>
+          <Card role="button" onClick={() => inputStore.setInputMethod("mouse")}>
             <Card.Img variant="top" src="/mouse.png" alt="Computer mouse symbol" />
             <Card.Body>
               <Card.Title className="text-center">Mouse</Card.Title>
@@ -37,7 +37,7 @@ function InputSelect() {
         </Col>
 
         <Col>
-          <Card role="button" onClick={() => inputStore.setInputMethod('touch')}>
+          <Card role="button" onClick={() => inputStore.setInputMethod("touch")}>
             <Card.Img variant="top" src="/touch.png" alt="Touch symbol" />
             <Card.Body>
               <Card.Title className="text-center">Touch</Card.Title>
