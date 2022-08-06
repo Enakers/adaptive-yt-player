d7ef44c858b5b00d51fff0b8136bd9d1bee3e19c
EnakNalla <enak.nalla@gmail.com>
2022-08-06 13:21:57 +0100

    fix: set referrer-policy
    This is required for some yt embed requests.

--- next.config.js
+++ next.config.js
@@ -9,30 +9,32 @@ const nextConfig = {
     {
       source: "/:path*",
       headers: [
-        // ...nextSafe({
-        //   isDev,
-        //   contentSecurityPolicy: {
-        //     "img-src": [
-        //       "'self'",
-        //       "data:",
-        //       "https://lh3.googleusercontent.com",
-        //       "https://i.ytimg.com",
-        //       "https://yt3.ggpht.com"
-        //     ],
-        //     "connect-src": ["'self'", "https://www.googleapis.com"],
-        //     "script-src": ["'self'", "https://www.youtube.com"],
-        //     "frame-src": ["'self'", "https://www.youtube-nocookie.com"],
-        //     "style-src": isDev
-        //       ? []
-        //       : ["'self'", "'sha256-+ggG/X5/BziMV+92NuWVuGj96UXUoD0vzVERtSh8Nv4='"]
-        //   },
-        //   permissionsPolicy: {
-        //     fullscreen: ["'self'"]
-        //   }
-        // }),
+        ...nextSafe({
+          isDev,
+          contentSecurityPolicy: {
+            "img-src": [
+              "'self'",
+              "data:",
+              "https://lh3.googleusercontent.com",
+              "https://i.ytimg.com",
+              "https://yt3.ggpht.com"
+            ],
+            "connect-src": ["'self'", "https://www.googleapis.com"],
+            "script-src": ["'self'", "https://www.youtube.com"],
+            "frame-src": ["'self'", "https://www.youtube-nocookie.com"],
+            "style-src": isDev
+              ? []
+              : ["'self'", "'sha256-+ggG/X5/BziMV+92NuWVuGj96UXUoD0vzVERtSh8Nv4='"]
+          },
+          xssProtection: false
+        }),
         {
           key: "Strict-Transport-Security",
           value: "max-age=31536000; includeSubDomains; preload"
+        },
+        {
+          key: "Referrer-Policy",
+          value: "origin-when-cross-origin"
         }
       ]
     }
