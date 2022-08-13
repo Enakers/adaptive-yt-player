bb618801d6c30a30d6b6cc050edfcf808307e020
EnakNalla <enak.nalla@gmail.com>
2022-08-13 13:41:19 +0100

    build: remove security headers

--- next.config.js
+++ next.config.js
@@ -4,48 +4,48 @@ const isDev = process.env.NODE_ENV !== "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  headers: () => [
-    {
-      source: "/:path*",
-      headers: [
-        ...nextSafe({
-          isDev,
-          contentSecurityPolicy: {
-            "img-src": [
-              "'self'",
-              "data:",
-              "https://lh3.googleusercontent.com",
-              "https://i.ytimg.com",
-              "https://yt3.ggpht.com"
-            ],
-            "connect-src": ["'self'", "https://www.googleapis.com"],
-            "script-src": ["'self'", "https://www.youtube.com"],
-            "frame-src": ["'self'", "https://www.youtube-nocookie.com"],
-            "style-src": isDev
-              ? []
-              : [
-                  "'self'",
-                  "'sha256-+ggG/X5/BziMV+92NuWVuGj96UXUoD0vzVERtSh8Nv4='",
-                  "sha256-KkLBTP2id6c7TuTX4KBzGRSZ1GQyIsiVeQF9yWOfUR0="
-                ],
-            "form-action": ["'self'", "https://yt.enak-nalla.dev/api/auth/signin/google"]
-          },
-          permissionsPolicy: {
-            fullscreen: ["'self'"]
-          }
-        }),
-        {
-          key: "Strict-Transport-Security",
-          value: "max-age=31536000; includeSubDomains; preload"
-        },
-        {
-          key: "Referrer-Policy",
-          value: "origin-when-cross-origin"
-        }
-      ]
-    }
-  ]
+  reactStrictMode: true
+  // headers: () => [
+  //   {
+  //     source: "/:path*",
+  //     headers: [
+  //       ...nextSafe({
+  //         isDev,
+  //         contentSecurityPolicy: {
+  //           "img-src": [
+  //             "'self'",
+  //             "data:",
+  //             "https://lh3.googleusercontent.com",
+  //             "https://i.ytimg.com",
+  //             "https://yt3.ggpht.com"
+  //           ],
+  //           "connect-src": ["'self'", "https://www.googleapis.com"],
+  //           "script-src": ["'self'", "https://www.youtube.com"],
+  //           "frame-src": ["'self'", "https://www.youtube-nocookie.com"],
+  //           "style-src": isDev
+  //             ? []
+  //             : [
+  //                 "'self'",
+  //                 "'sha256-+ggG/X5/BziMV+92NuWVuGj96UXUoD0vzVERtSh8Nv4='",
+  //                 "sha256-KkLBTP2id6c7TuTX4KBzGRSZ1GQyIsiVeQF9yWOfUR0="
+  //               ],
+  //           "form-action": ["'self'", "https://yt.enak-nalla.dev/api/auth/signin/google"]
+  //         },
+  //         permissionsPolicy: {
+  //           fullscreen: ["'self'"]
+  //         }
+  //       }),
+  //       {
+  //         key: "Strict-Transport-Security",
+  //         value: "max-age=31536000; includeSubDomains; preload"
+  //       },
+  //       {
+  //         key: "Referrer-Policy",
+  //         value: "origin-when-cross-origin"
+  //       }
+  //     ]
+  //   }
+  // ]
 };
 
 module.exports = nextConfig;
